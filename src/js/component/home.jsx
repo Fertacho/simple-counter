c7af064cfd3a9198eb081d598b29ad9fc23a5704
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-05-27 01:47:18 +0000

    avance

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,8 +6,16 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<p>asdasd</p>
+		<div className="SecCounter">
+			<diV className="clock">
+				<i className="fa-light fa-clock"></i>
+			</diV>
+			<div className="Cmil"></div>
+			<div className="Dmil"></div>
+			<div className="Umil"></div>
+			<div className="centena"></div>
+			<div className="decena"></div>
+			<div className="Unidad"></div>
 		</div>
 	);
 };
